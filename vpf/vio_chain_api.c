d9334bf602e0cdaeb19b11036a4c2a3ade7b061f
wenbo.he <wenbo.he@horizon.cc>
2024-09-24 11:28:32 +0800

    media: vpf: fix [X5-1668] Reduce metadata overflow loglevel
    Change-Id: I3ad3cc2a16956d9075ff5d934d4999d2e3465be9
    Signed-off-by: wenbo.he <wenbo.he@horizon.cc>

--- vpf/vio_chain_api.c
+++ vpf/vio_chain_api.c
@@ -417,7 +417,8 @@ void *vio_get_metadata(u32 flow_id, u32 frame_id)
 		meta_mgr->max_frameid = frame_id;
 	} else {
 		if ((meta_mgr->max_frameid - frame_id) >= META_OVERFLOW_EWARN)
-			vio_warn_ratelimited("%s: overflow early warning,"
+			/* metadata not used, using debug level log */
+			vio_dbg("%s: overflow early warning,"
 					"max frameid %d, frame id %d, please check!\n",
 					__func__, meta_mgr->max_frameid, frame_id);
 	}
@@ -430,7 +431,8 @@ void *vio_get_metadata(u32 flow_id, u32 frame_id)
 	} else if (meta_mgr->frameid[cur_index] == frame_id) {
 		metadata = meta_mgr->metadata + cur_index * METADATA_SIZE;
 	} else {
-		vio_warn_ratelimited("%s: frame id %d not match\n", __func__, frame_id);
+		/* medata not used, using debug level log */
+		vio_dbg("%s: frame id %d not match\n", __func__, frame_id);
 	}
 	vio_x_barrier_irqr(meta_mgr, flags);
 
